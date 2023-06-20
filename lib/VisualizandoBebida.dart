ddbd7cfeae17bcc036976ae26e4cdb31b122ca72
FranLustosa <franciellelimalustosa>
2023-06-20 09:29:59 -0300

    Alterações e implementações

--- lib/VisualizandoBebida.dart
+++ lib/VisualizandoBebida.dart
@@ -1,6 +1,14 @@
-// --- PÁGINA DE VISUALIZAR BEBIDA E ADICIONAR AO CARRINHO --- //
+// --- PÁGINA DOS MEUS BOTÕES DE NAVEGAÇÃO INFERIOR, É CHAMADO PELA AS OUTRAS CLASSES --- //
+// --- INTERLIGADA A TODAS AS PÁGINAS QUE É CHAMADO --- //
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_login/AdicionadoAoCarrinho.dart';
+import 'package:flutter_application_login/BebidasAlcoolicas.dart';
+import 'package:flutter_application_login/Inicial.dart';
+import 'package:flutter_application_login/Login.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/rendering.dart';
+import 'CarrinhoDeCompras.dart';
 
 class VisualizandoBebida extends StatefulWidget {
   const VisualizandoBebida({Key? key}) : super(key: key);
@@ -53,7 +61,8 @@ class _VisualizandoBebidaState extends State<VisualizandoBebida> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Positioned(
-                        top: 100, // Defina a posição vertical desejada
+                        top: 300,
+                        left: 0,
                         child: SizedBox(
                           width: 200,
                           height: 50,
@@ -69,6 +78,7 @@ class _VisualizandoBebidaState extends State<VisualizandoBebida> {
                                 ),
                               ],
                             ),
+                            // CÓDIGO INCREMENTADOR
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                               children: [
@@ -109,15 +119,16 @@ class _VisualizandoBebidaState extends State<VisualizandoBebida> {
                               context: context,
                               builder: (BuildContext context) {
                                 return AlertDialog(
+                                  actionsOverflowAlignment:
+                                      OverflowBarAlignment.center,
                                   title: Text(
                                       'Adicionado ao carrinho com sucesso!'),
                                   actions: [
                                     ElevatedButton(
                                       onPressed: () {
-                                        // Lógica para adicionar ao carrinho
                                         Navigator.of(context).pop();
                                       },
-                                      child: Text('Adicionar ao carrinho'),
+                                      child: Text('Continuar Pedindo'),
                                       style: ElevatedButton.styleFrom(
                                         fixedSize: Size(250, 40),
                                         backgroundColor: Color(0xFF00265F),
@@ -131,14 +142,18 @@ class _VisualizandoBebidaState extends State<VisualizandoBebida> {
                                     SizedBox(height: 16),
                                     ElevatedButton(
                                       onPressed: () {
-                                        Navigator.of(context).pop();
+                                        Navigator.of(context).push(
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    CarrinhoDeCompras()));
                                       },
-                                      child: Text('Continuar comprando'),
+                                      child: Text('Ver carrinho'),
                                       style: ElevatedButton.styleFrom(
                                         fixedSize: Size(250, 40),
-                                        backgroundColor: Colors.white,
-                                        primary: Color(0xFF00265F),
-                                        onPrimary: Color(0xFF00265F),
+                                        backgroundColor:
+                                            Color.fromARGB(255, 85, 85, 85),
+                                        primary:
+                                            Color.fromARGB(255, 255, 255, 255),
                                         shape: RoundedRectangleBorder(
                                           borderRadius:
                                               BorderRadius.circular(15),
@@ -177,6 +192,62 @@ class _VisualizandoBebidaState extends State<VisualizandoBebida> {
                 child: Image.asset(
                   'assets/images/bebida.png',
                   fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 37,
+            top: 320,
+            child: Align(
+              child: SizedBox(
+                width: 222,
+                height: 32,
+                child: Text(
+                  'Cerveja Heineken 600ml',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    height: 1.5599022865,
+                    letterSpacing: -0.6,
+                    color: Color(0xff4f4f4f),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 37,
+            top: 350,
+            child: Align(
+              child: SizedBox(
+                width: 328,
+                height: 71,
+                child: Text(
+                  'Lorem ipsum dolor sit amet consectetur. Tempus interdum egestas vitae tellus parturient nam risus sed. Euismod tempor nunc eu est orci aenean in. ',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400,
+                    height: 1.5599023183,
+                    letterSpacing: -0.45,
+                    color: Color(0xff4f4f4f),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 250,
+            top: 320,
+            child: Align(
+              child: SizedBox(
+                width: 92,
+                height: 26,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xb200255e),
+                  ),
                 ),
               ),
             ),
